506a8cd81b748d653cb27bf9377567dccfa9111a
Mohsin jameel <qureshimohsinjameel@gmail.com>
2026-01-24 21:55:16 +0500

    react project created and added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl bg-red-500 text-white">This is mj</h1>
+    </div>
+  );
+}
+
+export default App;
